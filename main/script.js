62ba19e89ee4e969267f2b78a23f1b9c6a0f062d
Ksardas21 <Ksardas21@github.com>
2020-09-11 07:30:30 +0400

    Delta Board

--- main/script.js
+++ main/script.js
@@ -3,7 +3,17 @@ function darkMode(){
 }
 function dropDown(){
     var options = document.getElementById("options")
-    options.style.display == "block" ? options.style.display = "none" : 
-    options.style.display = "block"; 
     options.classList.toggle("active")
 }
+window.onclick = function(event) {
+    if (!event.target.matches(['.header-nav', '.profile-image', '.top-profile-name', '.dropdown-icon'])) {
+      var dropdowns = document.getElementsByClassName("top-profile-menu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('active')) {
+          openDropdown.classList.remove('active');
+        }
+      }
+    }
+  }
