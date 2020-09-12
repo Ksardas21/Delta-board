eed0d4988623df288e93e133137e5308b1832000
Ksardas21 <Ksardas21@github.com>
2020-09-13 03:05:32 +0400

    delta board

--- main/script.js
+++ main/script.js
@@ -1,12 +1,12 @@
 function darkMode(){
-    document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("dark-mode");
 }
 function dropDown(){
     var options = document.getElementById("options")
     options.classList.toggle("active")
 }
 window.onclick = function(event) {
-    if (!event.target.matches(['.header-nav', '.profile-image', '.top-profile-name', '.dropdown-icon'])) {
+    if (!event.target.matches(['.header-nav', '.profile-image', '.top-profile-name', '.dropdown-icon', '.top-profile-menu', '.setting-buttons', '.setting-icons', '.profile-parameters',])) {
       var dropdowns = document.getElementsByClassName("top-profile-menu");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
