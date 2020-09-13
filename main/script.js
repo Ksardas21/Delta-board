3e6eacb5fecd69f62d608a628b3f5d0cef3a86db
Ksardas21 <Ksardas21@github.com>
2020-09-13 23:30:41 +0400

    delta board

--- main/script.js
+++ main/script.js
@@ -6,7 +6,8 @@ function dropDown(){
     options.classList.toggle("active")
 }
 window.onclick = function(event) {
-    if (!event.target.matches(['.header-nav', '.profile-image', '.top-profile-name', '.dropdown-icon', '.top-profile-menu', '.setting-buttons', '.setting-icons', '.profile-parameters',])) {
+    if (!event.target.matches(['.header-nav', '.profile-image', '.top-profile-name',
+     '.dropdown-icon', '.top-profile-menu', '.setting-buttons', '.setting-icons', '.profile-parameters',])) {
       var dropdowns = document.getElementsByClassName("top-profile-menu");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
