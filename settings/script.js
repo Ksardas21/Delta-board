3863b134e015e42f4105b900492ade7474305099
Ksardas21 <Ksardas21@github.com>
2020-09-16 00:19:24 +0400

    delta board

---
+++ settings/script.js
@@ -0,0 +1,20 @@
+function darkMode(){
+    document.body.classList.toggle("dark-mode");
+  }
+  function dropDown(){
+      var options = document.getElementById("options")
+      options.classList.toggle("active")
+  }
+  window.onclick = function(event) {
+      if (!event.target.matches(['.header-nav', '.profile-image', '.top-profile-name',
+       '.dropdown-icon', '.top-profile-menu', '.setting-buttons', '.setting-icons', '.profile-parameters',])) {
+        var dropdowns = document.getElementsByClassName("top-profile-menu");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('active')) {
+            openDropdown.classList.remove('active');
+          }
+        }
+      }
+    }
